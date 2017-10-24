77097f7b9eeedffacbd1b25dc8c110707489af92
maskerk <1130600679@qq.com>
2017-10-24 16:25:17 +0000

    除了随机生成 基本完成 尚未测试

--- forth_homework/match.cpp
+++ forth_homework/match.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -17,9 +19,9 @@ class Department
 {
 public:
 	Department(){}
-	Department(string name_part,int tip_limit,vector<string> tag,vector<string> routine)
+	Department(string name_part,int top_limit,vector<string> tag,vector<string> routine)
 	{
-		this->top_limit = tip_limit;
+		this->top_limit = top_limit;
 		this->name_part = name_part;
 		vector<string>::iterator iter;
 		for(iter = tag.begin();iter!=tag.end();iter++)
@@ -29,9 +31,18 @@ public:
 			this->routine.push_back(*iter);
 
 		//num_stu_in_part++;
-
 	}
 
+	bool addStu(string id)
+	{
+		if(num_stu+1 <= top_limit)
+		{
+			++num_stu;
+			this->id.push_back(id);
+			return true;
+		}
+		return false;
+	}
 	int getTagLength()
 	{
 		vector<string>::iterator iter;
@@ -55,11 +66,21 @@ public:
 	{
 		return name_part;
 	}
+	vector<string> getRoutine()
+	{
+		return routine;
+	}
+	vector<string> getID()
+	{
+		return id;
+	}
 private:
+	int num_stu = 0;
 	int top_limit,num_stu_in_part = 0;
 	vector<string> tag;
 	vector<string> routine;
 	string name_part; 
+	vector<string> id;
 
 };
 
@@ -68,11 +89,13 @@ class Student
 {
 public:
 	Student(){}
-	Student(int grade,vector<string> interest,vector<string> sdt_dpt,vector<string> free_time)
+	Student(string id,float grade,vector<string> interest,vector<string> sdt_dpt,vector<string> free_time)
+	//Student(id,grade,interest,sdt_dpt,free_time);
 	{
 		num_free_time = 0;
 		vector<string>::iterator iter_str;
 		this->grade = grade;
+		this->id = id;
 		for(iter_str = interest.begin();iter_str != interest.end();iter_str++)
 		{
 			this->interest = interest;
@@ -92,9 +115,9 @@ public:
 
 	}
 
-	vector<string> getSdtDpt()
+	vector<string> * getSdtDpt()
 	{
-		return sdt_dpt;
+		return & sdt_dpt;
 	}
 
 	int getNumFreeTime()
@@ -113,14 +136,27 @@ public:
 	{
 		return num_depart;
 	}
+	vector<string> getFreeTime()
+	{
+		return free_time;
+	}
+	float getGrade()
+	{
+		return grade;
+	}
+	string getID()
+	{
+		return id;
+	}
 private:
-	int grade;//绩点
+	float grade;//绩点
 	int num_depart = 0;//////////////////////////////////
 	int num_free_time,num_sdt_dpt = 0;
 	vector<string> interest;
 	//vector<Department> sdt_dpt;//学生选择部门
 	vector<string> sdt_dpt;//学生选择部门
 	vector<string> free_time;//空余时间段
+	string id;
 };
 
 
@@ -171,14 +207,19 @@ void set_Department()
 
 void set_Student()
 {
-	int grade,num_interest,num_intest,num_sdt_dpt,num_free_time;//绩点
+	int num_interest,num_intest,num_sdt_dpt,num_free_time;
+	float grade;
 	vector<string> interest;
+	string id;
 	vector<string> sdt_dpt;//学生选择部门
 	vector<string> free_time;//空余时间段
 	int j;
 	string str;
+	string str1;
 	for(int i = 0;i < N_student;i++)
 	{
+		cout << "请输入第" << i << "个学生的学号：" << endl;
+		cin >> str1;
 		cout << "请输入第" << i << "个学生的学分绩点：" << endl;
 		cin >> grade;
 		cout << "请输入第" << i << "个学生的意愿部门个数：" << endl;
@@ -209,59 +250,62 @@ void set_Student()
 		}
 
 		//创建该对象
-		stu[i] = new Student(grade,interest,sdt_dpt,free_time);
+		stu[i] = new Student(id,grade,interest,sdt_dpt,free_time);
 
 	}
 
 }
 
-void match()
+void matchFree()
 {
 	//查看所有学生的空余时间是不是匹配自己申报部门的例会时间 如果不符合就排除该意愿
 	int i,j,k,l;
-	vector<string> sdt_dpt;
+	vector<string> *sdt_dpt;
+
+	vector<string> free_time;
+	vector<string> routine;
 	for(i = 0;i < N_student;i++)
 	{
 		int num_free_time = stu[i]->getNumFreeTime();//空余时间片段个数
 		int num_sdt_dpt = stu[i]->getNumSdtDpt();//意愿部门个数
 
 		sdt_dpt = stu[i]->getSdtDpt();//第i个学生的部门意愿向量
-
-
+		free_time = stu[i]->getFreeTime();
 
 		//判断是否所有空余时间片段有没有对任意一个部门的所有例会都不适合的 有的话就排除该部门意愿
-		for(j = 0;j < num_sdt_dpt;j++)//意愿部门个数循环
-		{
-			int pass = 1;
+		//for(j = 0;j < num_sdt_dpt;j++)//意愿部门个数循环
+		//{
 			//具体意愿部门例会时间个数循环 
 
 			vector<string>::iterator iter;
-			for(iter = sdt_dpt.begin();iter != sdt_dpt.end();iter++)
+			vector<string>::iterator iter2;
+			vector<string>::iterator iter3;
+			for(iter = sdt_dpt->begin();iter != sdt_dpt->end();iter++)
 			{
 				for(k = 0;k < N_department;k++)
 				{
+					
 					if(*iter == part[i]->getParName())//如果意愿部门就是该部门
 					{
+						routine = part[i]->getRoutine();
 						//查询空余时间是否包含该部门所有例会时间
-						for(k = 0;k < num_free_time;k++)
+						for(iter3 = routine.begin();iter3 != routine.end();iter3++)//例会时间片段
 						{
-							//字符串匹配判断
+							iter2 = find(free_time.begin(),free_time.end(),*iter3);
+							if(iter2 != free_time.end())//在空余时间段中
+							{
+								continue;//检查下一个例会片段
+							}
+							else//不在空余时间段中  排除该志愿
+							{
+								sdt_dpt->erase(iter);
+							}
 						}
-						//如果全部包含 则通过 否则 则去除该志愿
-						break;
 					}
 				}
 			}
-			for(k = 0;k < num_free_time;k++)//空余时间片段个数循环
-			{
-
-			}
-			if(!pass)//如果空余时间片段不能包含该部门例会时间 排除该部门意愿
-			{
-
-			}
-		}
 	}
+
 
 	//查询该部门是不是达到上限
 	//查询该学生意愿是不是超出
@@ -273,13 +317,63 @@ void match()
 	//
 	//
 
+
+
 }
 
+void byGrade()
+{
+	map<float,Student*> rank_grade;
+	vector<string> * vec;
+	Student * stu_i;
+	int i;
+	for(i = 0;i < N_student;i++)
+	{
+		rank_grade.insert(pair<float,Student*>(stu[i]->getGrade(),stu[i]));
+	}
+
+	map<float,Student*>::reverse_iterator riter;
+	vector<string>::iterator iter2;
+	for(riter = rank_grade.rbegin();riter != rank_grade.rend() ;riter++)
+	{
+		stu_i = riter->second;
+		vec = stu_i->getSdtDpt();
+		for(iter2 = vec->begin();iter2 != vec->end();iter2++)
+		{
+			for(int j = 0;j < N_department;j++)
+			{
+				if(*iter2 == part[j]->getParName())
+				{
+					if(part[j]->addStu(stu_i->getID()))//进入部门&&如果进入部门返回成功
+					{
+						//(stu_i->getSdtDpt)->erase(iter2);
+						vec->erase(iter2);//删除该学生的该志愿
+					}
+				}
+			}
+		}
+
+	}
+}
+
+void outputDepartment()
+{
+	vector<string> id;
+	vector<string>::iterator id_iter;
+	for(int i = 0;i < N_department;i++)
+	{
+		id = part[i]->getID();
+		for(id_iter = id.begin();id_iter != id.end();id_iter++)
+		{
+			cout << *id_iter << endl;
+		}
+	}
+}
 int main()
 {
 	set_Department();//部门信息录入
 	set_Student();//学生信息录入
-	match();
-
+	matchFree();
+	outputDepartment();
 	return 0;
 }
